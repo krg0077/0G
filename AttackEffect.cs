f035c5dec40dcbe40a76be7c4443b2a14871b392
k76 <krg0077++src@gmail.com>
2021-03-16 04:33:06 -0700

    Add abstract AttackEffect class

---
+++ AttackEffect.cs
@@ -0,0 +1,15 @@
+﻿using _0G.Legacy;
+using UnityEngine;
+
+namespace _0G
+{
+    public abstract class AttackEffect : MonoBehaviour
+    {
+        protected Attack m_Attack;
+
+        private void Awake()
+        {
+            m_Attack = GetComponent<Attack>();
+        }
+    }
+}
