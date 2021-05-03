krg0077/0G

commit 87f182b0d13693de0d8cc816fec989a4d223829a

Fix null/missing reference exceptions in GraphicController

Legacy/GraphicController.cs
@@ -276,7 +276,7 @@ namespace _0G.Legacy
 
         public void RefreshAnimationImage()
         {
-            if (!RasterAnimation.IsSet()) return;
+            if (!RasterAnimation.IsSet() || RasterAnimation.Textures == null) return;
             m_AnimationImageIndex = Mathf.Min(m_AnimationImageIndex, m_AnimationImageCount - 1);
             SetTexture(RasterAnimation.Textures[m_AnimationImageIndex]);
         }
@@ -632,7 +632,8 @@ namespace _0G.Legacy
                 }
             }
 
-            if (animationName != RasterAnimation?.name)
+            // ReSharper disable once Unity.PerformanceCriticalCodeNullComparison
+            if (RasterAnimation != null && animationName != RasterAnimation.name)
             {
                 SetAnimation(context, animationName);
             }

commit 83b83e967176f21decbbb2c8a4f4eea40fb48a1c

Fix animation-setting logic in GraphicController
- Fix a bug introduced in 87f182b0d13693de0d8cc816fec989a4d223829a

Legacy/GraphicController.cs
@@ -633,7 +633,7 @@ namespace _0G.Legacy
             }
 
             // ReSharper disable once Unity.PerformanceCriticalCodeNullComparison
-            if (RasterAnimation != null && animationName != RasterAnimation.name)
+            if (RasterAnimation == null || animationName != RasterAnimation.name)
             {
                 SetAnimation(context, animationName);
             }